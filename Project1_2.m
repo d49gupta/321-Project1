93432c3cdabd4dcc05ef1c08794a0a4f92fd5566
Dharma Gupta <dharma.anita@gmail.com>
2024-02-17 09:31:32 -0500

    loop 2 works now

--- Project1_2.m
+++ Project1_2.m
@@ -6,18 +6,31 @@ b2 = r2/2;
 b3 = r3/3;
 b6 = 4;
 
-r5 = 0; % Placeholder
-theta6 = 0; % Placeholder
-theta6_vel = 0; % Placeholder
-theta6_acc = 0; % Placeholder
+% Masses of Links and sliders (g)
+m2 = 2.651;
+m3 = 8.482;
+m4 = 5;
+m5 = 5;
+m6 = 4.241;
 
 % Known/Symbolic Angles
 syms theta2_pos;
 theta2_vel = 135*6;
-theta3_pos= asind((-r2/r3)*sind(theta2_pos)) + 180;
+phi = 14.931;
+
+% Functions
+theta3_pos = asind((-r2/r3)*sind(theta2_pos)) + 180;
 theta3_vel = -r2*theta2_vel*cosd(theta2_pos)/(r3*cosd(theta3_pos));
 theta3_acc = (r2*(theta2_vel^2)*sind(theta2_pos) + r3*(theta3_vel^2)*sind(theta3_pos))/(r3*cosd(theta3_pos));
-phi = 14.931;
+
+r1_pos = (r2*cosd(theta2_pos) + r3*cosd(theta3_pos));
+r1_vel = (-r2*theta2_vel*sind(theta2_pos) - r3*theta3_vel*sind(theta3_pos));
+r1_acc = (-r2*(theta2_vel^2)*cosd(theta2_pos) - r3*(theta3_acc*sind(theta3_pos) + (theta3_vel^2)*cosd(theta3_pos)));
+
+r5 = 0; % Placeholder
+theta6 = 0; % Placeholder
+theta6_vel = 0; % Placeholder
+theta6_acc = 0; % Placeholder
 
 % Coefficients of Forces
 A44 = -r2*sind(theta2_pos);
@@ -28,13 +41,9 @@ A65 = -b3*cosd(theta3_pos);
 A64 = -b3*sind(theta3_pos);
 A610 = -(b3-r5)*cos(theta3_pos);
 A611 = -(b3-r5)*sin(theta3_pos);
+A1210 = tand(theta3_pos);
 A1514 = -r6*sind(theta6+phi);
 A1515 = r6*cosd(theta6+phi);
-
-% Masses of Links (g)
-m2 = 2.651;
-m3 = 8.482;
-m6 = 4.241;
 
 % Acceleration and MOI
 AG2X = -b2*(theta2_vel^2)*cosd(theta2_pos);
@@ -43,7 +52,8 @@ AG3X = -r2*(theta2_vel^2)*cosd(theta2_pos) - b3*theta3_acc*sind(theta3_pos) - b3
 AG3Y = -r2*(theta2_vel^2)*sind(theta2_pos) + b3*theta3_acc*cosd(theta3_pos) - b3*(theta3_vel^2)*sind(theta3_pos);
 IG3 = (1/12)*m3*r3;
 AG4X = r1_acc;
-AG5X = -r6*theta6_acc*sind(theta6) - r6*(theta6_vel^2)*cosd(theta6) + r6*theta6_acc*cosd(theta6) - r6*(theta6_vel^2)*sind(theta6); % Need theta6 and their derivatives
+AG5X = -r6*theta6_acc*sind(theta6) - r6*(theta6_vel^2)*cosd(theta6); % Need theta6 and their derivatives
+AG5Y = r6*theta6_acc*cosd(theta6) - r6*(theta6_vel^2)*sind(theta6); % Need theta6 and their derivatives
 AG6X = -b6*theta6_acc*sind(theta6) - b6*(theta6_vel^2)*cosd(theta6); % Need theta6 and their derivatives
 AG6Y = b6*theta6_acc*cosd(theta6) - b6*(theta6_vel^2)*sind(theta6); % Need theta6 and their derivatives
 IG6 = (1/12)*m6*r6;
@@ -56,30 +66,12 @@ A = [1	0	0	1	0	0	0	0	0	0	0	0	0	0  0;
     0	0	0	A64	A65	A66	A67	0	0	A610	A611	0	0	0	0;
     0	0	0	0	0	1	0	0	0	0	0	0	0	0	0;
     0	0	0	0	0	0	1	0	1	0	0	0	0	0	0;
-    0	0	0	0	0	0	0	0	0	0	0	0	0	0	0;
     0	0	0	0	0	0	0	0	0	1	0	0	0	1	0;
     0	0	0	0	0	0	0	0	0	0	1	0	0	0	1;
-    0	0	0	0	0	0	0	0	0	0	0	0	0	0	0;
+    0	0	0	0	0	0	0	0	0	A1210	-1	0	0	0	0;
     0	0	0	0	0	0	0	0	0	0	0	-1	0	1	0;
     0	0	0	0	0	0	0	0	0	0	0	0	-1	0	1;
     0	0	0	0	0	0	0	0	0	0	0	0	0	A1514	A1515;
-    ];
-
-forces = [F12x;
-         F12y;
-         M12;
-         F23x;
-         F23y
-         F34x;
-         F34y;
-         F14x;
-         F14y;
-         F16x;
-         F16y;
-         F35x;
-         F35y;
-         F56x;
-         F56y;
     ];
 
 results = [m2*AG2X; 
@@ -90,14 +82,33 @@ results = [m2*AG2X;
             IG3*theta3_acc;
             m4*AG4X;
             0;
-            0;
             m5*AG5X;
             m5*AG5Y;
             0;
             m6*AG6X;
             m6*AG6Y;
-            (IG6+m6*(b6)^2)*theta6_acc;
+            (IG6-m6*(b6)^2)*theta6_acc;
     ];
 
 disp(A);
-forces = linsolve(A, results);
+forces = linsolve(A, results); % Solves all unknown joint forces and M12
+
+% Shaking Force
+% FSX = -F16X + F12X;
+% FSY = -F16Y - F14Y - F12Y;
+% FS = sqrt(FSX^2 + FSY^2);
+
+% Shaking Moment
+% SM = F14Y*r1_pos - 4*F16X + 15*F16Y + M12;
+
+% Plot Forces
+% theta2_deg = 0:1:360;
+% for i = 1:length(forces)
+%     subplot(3, 5, i); % 3x5 grid of subplots
+%     force_values = arrayfun(forces{i}, theta2_rad); % Evaluate the ith force function over theta2
+%     plot(theta2_deg, force_values); % Plot force as a function of theta2 in degrees
+%     title(['Force ', num2str(i)]);
+%     xlabel('Theta2 (degrees)');
+%     ylabel('Force');
+% end
+
--- project1_1Animate.m
+++ project1_1Animate.m
@@ -6,21 +6,18 @@ r6 = 8;
 theta2_vel = 135*6;
 phi = 14.931;
 
-for theta2_pos = 0:1:720
+for theta2_pos = 0:1:360
 theta3_pos = asind((-r2/r3).*sind(theta2_pos));
-theta3_pos = theta3_pos + 180;
+theta3_pos = theta3_pos - 180;
 r1_pos = r2.*cosd(theta2_pos) + r3.*cosd(theta3_pos); 
 
-theta6_pos = asind((r2*asind(theta2_pos - theta3_pos)-4*sind(theta3_pos) + r1_pos*cosd(theta3_pos))/r6) + theta3_pos;
-r5 = (r1_pos - r6.*sind(theta6_pos) - r2.*sind(theta2_pos))/sind(theta3_pos);
 
+% theta6_pos = asind((15*sind(theta3_pos) - 4*cosd(theta3_pos) - r2*sind(theta3_pos - theta2_pos))/r6) + theta3_pos;
+theta6_pos = asind(-(15*sind(theta3_pos) - 4*cosd(theta3_pos) - r2*sind(theta3_pos - theta2_pos))/r6) + theta3_pos;
+theta6_pos = abs(theta6_pos + 180);
+r5 = 5;
 
-% theta6_pos = asind((-r1_pos.*(cosd(theta3_pos) - sind(theta3_pos)) - r2.*sind(theta3_pos - theta2_pos))/r6) - theta3_pos;
-% r5 = (r1_pos - r6.*sind(theta6_pos) - r2.*sind(theta2_pos))/sind(theta3_pos);
-
-% theta6_pos = asind((cosd(theta3_pos)*(r1_pos*sind(phi) + r2*sin(theta2_pos)) - sind(theta3_pos)*(r1_pos*cosd(phi) + r2*cosd(theta2_pos)))/r6) + theta3_pos;
-% r5 = (sind(theta6_pos)*(r1_pos*cosd(phi) + r2*cosd(theta2_pos)) - cosd(theta6_pos)*(r1_pos*sind(phi) + r2*sind(theta2_pos)))/sind(theta3_pos - theta6_pos);
-
+disp(theta6_pos)
 figure(1)
 plot(0, 0, 'o')
 hold on
@@ -28,13 +25,14 @@ hold on
 X = [0 r2.*cosd(theta2_pos) r1_pos];
 Y = [0 r2.*sind(theta2_pos) 0];
 
-X1 = [0 r2.*cosd(theta2_pos) r5.*cosd(theta3_pos) -15];
-Y1 = [0 r2.*sind(theta2_pos) r5.*sind(theta3_pos) -4];
+X1 = [-15 (-15 + r6*cosd(theta6_pos)) r2.*cosd(theta2_pos) 0];
+Y1 = [-4 (-4 + r6*sind(theta6_pos)) r2.*sind(theta2_pos) 0];
+
 
 plot(r2.*cosd(theta2_pos), r2.*sind(theta2_pos), 'o')
 plot (r1_pos, 0, 'o')
-plot(r5.*cosd(theta3_pos), r5.*sind(theta3_pos), 'o')
 plot(-15, -4, 'o')
+plot(-15 + r6.*cosd(theta6_pos), -4 + r6.*sind(theta6_pos), 'o')
 hold on
 
 plot(X, Y, 'r');
